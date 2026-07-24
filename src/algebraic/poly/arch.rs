7e661cfc52f4e7614d3286d34338454ea4614bec
Deirdre Connolly <durumcrustulum@gmail.com>
2026-07-24 09:29:06 -0400

    avx2 vectorize compress/decompress NTT merge

--- src/algebraic/poly/arch.rs
+++ src/algebraic/poly/arch.rs
@@ -25,11 +25,9 @@ mod neon;
 #[cfg(mlkem_selkie_arch = "avx2")]
 use avx2::{basemul_accumulate, basemul_reduce};
 #[cfg(mlkem_selkie_arch = "avx2")]
-pub(crate) use avx2::{multiply, ntt, ntt_inverse};
+pub(crate) use avx2::{canonical, compress, decompress, multiply, ntt, ntt_inverse};
 #[cfg(not(any(mlkem_selkie_arch = "neon", mlkem_selkie_arch = "avx2")))]
 use generic::{basemul_accumulate, basemul_reduce};
-#[cfg(mlkem_selkie_arch = "avx2")]
-pub(crate) use generic::{canonical, compress, decompress};
 #[cfg(not(any(mlkem_selkie_arch = "neon", mlkem_selkie_arch = "avx2")))]
 pub(crate) use generic::{canonical, compress, decompress, multiply, ntt, ntt_inverse};
 #[cfg(mlkem_selkie_arch = "neon")]
